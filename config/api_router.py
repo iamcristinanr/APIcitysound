12bdd4e257d0cc58ae26fb85fedc8676cbe0d387
iamcristinanr <crisnieromero@gmail.com>
2024-04-13 12:39:16 +0200

    added Comment and own profile

--- config/api_router.py
+++ config/api_router.py
@@ -3,14 +3,14 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
 from citysound.users.api.views import UserViewSet
-from citysound.tours.api.views import TourViewSet, TourStopViewSet
+from citysound.tours.api.views import TourViewSet, TourStopViewSet, TourCommentViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
 router.register("tour", TourViewSet, TourStopViewSet)
 router.register(r"tours/(?P<tour_id>[^/.]+)/stops", TourStopViewSet, basename='tour-stops')
-
+router.register(r"tours/(?P<tour_id>[^/.]+)/comments", TourCommentViewSet, basename='tour-comments')
 
 app_name = "api"
 urlpatterns = router.urls
